5544ffb509e3cfc6dba0ea322326b2482c84e787
Ross Porter <rossporter506@gmail.com>
2023-09-28 14:23:22 +1300

    Linux build scripts

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+PATH=$PATH:./msp430-gcc/bin cargo build "$@"
--- run.sh
+++ run.sh
@@ -1,2 +1,14 @@
-#!/usr/bin/env bash
-mspdebug --allow-fw-update tilib "prog $1 & run"
+#!/bin/bash
+
+PATH=$PATH:./msp430-gcc/bin cargo build "$@"
+
+BIN_PATH="./target/msp430-none-elf/debug/msp430_pcb_self_test"
+for arg in "$@"
+do
+    if [ $arg = "--release" ]
+    then
+        BIN_PATH="./target/msp430-none-elf/release/msp430_pcb_self_test"
+    fi
+done
+
+./uniflash/dslite.sh --config=./uniflash/user_files/configs/MSP430FR2355.ccxml -u $BIN_PATH
