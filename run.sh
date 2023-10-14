42309b24f9799b85144290c043a76b6d90e7e2c7
AlexApps99 <alex.apps99@gmail.com>
2023-10-14 23:57:52 +1300

    Got gdb working, both in CLI and VSCode. Replace `rf2500` with `tilib` for launchpad devboard.

--- run.sh
+++ run.sh
@@ -3,12 +3,28 @@
 # Stop when a command fails
 set -e
 
-echo "Flashing..."
-./uniflash/dslite.sh --config=./uniflash/user_files/configs/MSP430FR2355.ccxml -u "$1"
+MSPDEBUG_DRIVER=rf2500
+# For the launchpad kit only
+#MSPDEBUG_DRIVER=tilib
 
-if [ "$2" == "debug" ]; then
+echo "Flashing..."
+if [ -e ./uniflash/dslite.sh ]; then
+    ./uniflash/dslite.sh --config=./uniflash/user_files/configs/MSP430FR2355.ccxml -u "$1"
+else
+    # Alternate flashing command
+    ./mspdebug/bin/mspdebug $MSPDEBUG_DRIVER "prog \"$1\"" "verify \"$1\""
+fi
+
+if [ "$2" == "gdb" ]; then
     echo "Starting debug daemon"
-    ./mspdebug/bin/mspdebug -C mspdebug.cfg rf2500
+    ./mspdebug/bin/mspdebug $MSPDEBUG_DRIVER gdb &
+
     echo "Starting GDB"
     ./msp430-gcc/bin/msp430-elf-gdb -x mspdebug.gdb "$1"
+
+    # Kill background debugger
+    pkill -P $$
+elif [ "$2" == "debug" ]; then
+    echo "Starting debug daemon headless"
+    ./mspdebug/bin/mspdebug $MSPDEBUG_DRIVER gdb
 fi
