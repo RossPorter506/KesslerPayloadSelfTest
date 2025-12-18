324540efb701b46d8a3dbd9c40e212a27cb019cf
Ross Porter <rpor545@UoA.auckland.ac.nz>
2025-12-18 14:30:57 +1300

    Cherrypick: Fix run.bat script for windows flashing
    errorlevel was being set prior to running conditional. Use delayed expansion to fix.

--- run.bat
+++ run.bat
@@ -1,9 +1,11 @@
 @echo off
+setlocal enabledelayedexpansion
+
 where /q dslite.bat
-if %errorlevel% neq 0 (
+if !errorlevel! neq 0 (
    echo Unable to find dslite.bat in your PATH. Assuming it's at .\uniflash\dslite.bat. If it isn't then this will fail.
    .\uniflash\dslite.bat --config=.\MSP430FR2355.ccxml -u %*
-   exit /B %errorlevel%
+   exit /B !errorlevel!
 )
 
 dslite.bat --config=.\MSP430FR2355.ccxml -u %*
